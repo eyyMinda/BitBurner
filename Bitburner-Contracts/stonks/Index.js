9b84d0304398a3280c5df21b99ff7215a01d286a
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-12-05 17:31:52 +0200

    stonks

--- Bitburner-Contracts/stonks/Index.js
+++ Bitburner-Contracts/stonks/Index.js
@@ -1,19 +1,42 @@
 const { Input } = require('./Input');
 
-function buyLowSellHigh(inp) {
-  const stonks = inp.split(',');
-  let low = stonks[0];
-  let high = stonks[0];
-  let bought = 0;
-  let profit = 0;
-  // for (let stonk of stonks) {
-  //   if (stonk < low) low = stonk;
-  //   if (stonk > high)  high = stonk;
-  //   if(stonk > bought && bought < high) profit += high-bought; bought = 0;
-  //   if(!bought){}
-  // }
-  console.log([159 - 44, 97 - 66, 120 - 30, 134 - 84, 177 - 88, 199 - 18, 189 - 101, 175 - 8, 196 - 113, 159 - 23].reduce((a, b) => a + eval(b), 0));
-  console.log('something: ', 123);
+function maxProfit(arrayData) {
+  let i, j, k;
+  let maxTrades = arrayData[0];
+  let stockPrices = arrayData[1];
+
+  // VERY EFFICIENTS YES OF COURSE
+  let tempStr = "[0";
+  for (i = 0; i < stockPrices.length; i++) {
+    tempStr += ",0";
+  }
+  tempStr += "]";
+  let tempArr = "[" + tempStr;
+  for (i = 0; i < maxTrades - 1; i++) {
+    tempArr += "," + tempStr;
+  }
+  tempArr += "]";
+  let highestProfit = JSON.parse(tempArr);
+
+  for (i = 0; i < maxTrades; i++) {
+    for (j = 0; j < stockPrices.length; j++) { // Buy / Start
+      for (k = j; k < stockPrices.length; k++) { // Sell / End
+        if (i > 0 && j > 0 && k > 0) {
+          highestProfit[i][k] = Math.max(highestProfit[i][k], highestProfit[i - 1][k], highestProfit[i][k - 1], highestProfit[i - 1][j - 1] + stockPrices[k] - stockPrices[j]);
+        } else if (i > 0 && j > 0) {
+          highestProfit[i][k] = Math.max(highestProfit[i][k], highestProfit[i - 1][k], highestProfit[i - 1][j - 1] + stockPrices[k] - stockPrices[j]);
+        } else if (i > 0 && k > 0) {
+          highestProfit[i][k] = Math.max(highestProfit[i][k], highestProfit[i - 1][k], highestProfit[i][k - 1], stockPrices[k] - stockPrices[j]);
+        } else if (j > 0 && k > 0) {
+          highestProfit[i][k] = Math.max(highestProfit[i][k], highestProfit[i][k - 1], stockPrices[k] - stockPrices[j]);
+        } else {
+          highestProfit[i][k] = Math.max(highestProfit[i][k], stockPrices[k] - stockPrices[j]);
+        }
+      }
+    }
+  }
+  return highestProfit[maxTrades - 1][stockPrices.length - 1];
 }
 
-buyLowSellHigh(Input); //Not Finished
+console.log(maxProfit([Math.ceil(Input.length / 2), Input])); //Trader II
+// console.log(maxProfit(Input));
